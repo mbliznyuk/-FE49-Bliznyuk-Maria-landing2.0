e84e29e069709e4ee0a0f2a5f4bdd3ea462867cd
Maria <maria.bliznuk18@gmail.com>
2023-05-09 15:28:27 +0300

    added adaptiv

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,42 @@
-new Swiper ('.swiper-container', {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      
-    loop: true,
+
+const swiper = new Swiper ('.swiper-container', {
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        
+      loop: true,
+  });
+
+// $ (document).ready(function() {
+// $('.burger').click(function(event) {
+//   $('.burger, .navigation, .logo-title').toggleClass('active');
+
+// })
+// })
+
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+  toggleBurger(
+    burger,
+    document.querySelector('.navigation'),
+    document.querySelector('.logo-title')
+  );
 });
 
+
+const toggleBurger = (...params) => {
+  params.forEach((elem) => {
+    if(elem.className.includes('active')) {
+      elem.classList.remove('active');
+    }
+    else {
+      elem.classList.add('active');
+    }
+  })
+};
