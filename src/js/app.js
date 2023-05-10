13882f4a4bad9b15bbb42dd00a40a8a7d0c73600
Maria <maria.bliznuk18@gmail.com>
2023-05-10 21:07:16 +0300

    added adaptiv

--- src/js/app.js
+++ src/js/app.js
@@ -12,31 +12,31 @@ const swiper = new Swiper ('.swiper-container', {
       loop: true,
   });
 
-// $ (document).ready(function() {
-// $('.burger').click(function(event) {
-//   $('.burger, .navigation, .logo-title').toggleClass('active');
+$ (document).ready(function() {
+$('.burger').click(function(event) {
+  $('.burger, .navigation, .logo-title').toggleClass('active');
 
-// })
-// })
+})
+})
 
-const burger = document.querySelector('.burger');
+// const burger = document.querySelector('.burger');
 
-burger.addEventListener('click', () => {
-  toggleBurger(
-    burger,
-    document.querySelector('.navigation'),
-    document.querySelector('.logo-title')
-  );
-});
+// burger.addEventListener('click', () => {
+//   toggleBurger(
+//     burger,
+//     document.querySelector('.navigation'),
+//     document.querySelector('.logo-title')
+//   );
+// });
 
 
-const toggleBurger = (...params) => {
-  params.forEach((elem) => {
-    if(elem.className.includes('active')) {
-      elem.classList.remove('active');
-    }
-    else {
-      elem.classList.add('active');
-    }
-  })
-};
+// const toggleBurger = (...params) => {
+//   params.forEach((elem) => {
+//     if(elem.className.includes('active')) {
+//       elem.classList.remove('active');
+//     }
+//     else {
+//       elem.classList.add('active');
+//     }
+//   })
+// };
